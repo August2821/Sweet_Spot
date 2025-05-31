39aa041075f6b3ca5d9480e81e8b11020bd562c4
August2821 <gosohanjinnunkkaebi@gmail.com>
2025-05-31 13:12:40 +0900

    일단 백엔드 완료

--- sweetspot-server/src/main/java/com/sweetspot/server/post/like/PostLikeRequestDto.java
+++ sweetspot-server/src/main/java/com/sweetspot/server/post/like/PostLikeRequestDto.java
@@ -1,17 +1,12 @@
-package com.sweetspot.server.comment;
+package com.sweetspot.server.post.like;
 
-public class CommentRequestDTO {
+public class PostLikeRequestDto {
     private Long userId;
     private Long postId;
-    private String content;
 
-    // Getters & Setters
     public Long getUserId() { return userId; }
     public void setUserId(Long userId) { this.userId = userId; }
 
     public Long getPostId() { return postId; }
     public void setPostId(Long postId) { this.postId = postId; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
 }
